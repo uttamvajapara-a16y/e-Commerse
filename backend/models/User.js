78501bb9d326f256f34bb129a729485c44c3d571
uttamvajapara-a16y <uttamvajapara@gmail.com>
2026-06-23 14:00:47 +0530

    userModel

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// User Schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
