d213f7edff8a77fc443b19805946ceb803de70b2
uttamvajapara-a16y <uttamvajapara@gmail.com>
2026-06-25 00:04:15 +0530

    error fixed

--- backend/index.js
+++ backend/index.js
@@ -18,27 +18,17 @@ app.use(cors(
 app.use(express.json()) ;
 app.use(express.urlencoded({extended:true})) ;
 
-app.get('/',(req,res)=>{
-    res.json({message:'hello from backend'}) ;
-}) ;
-
 app.use('/api/auth', require('./routes/authRoutes')) ;
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('eShopping API is running in Development mode...');
-  });
-}
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+});
 
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`)) ;
